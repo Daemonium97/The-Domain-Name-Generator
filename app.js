c67ca521b73ac8a1f8f473098ae7acdaeef8d259
Daemonium97 <daemonium97@gmail.com>
2021-01-27 16:46:51 +0000

    fixed some problems

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ let domain = ['.com', '.net', '.us', '.io'];
 
 
 function ranDom() {
-    var container = pronoun[Math.floor(Math.random() * pronoun.length)] + adj[Math.floor(Math.random() * adj.length)] + noun[Math.floor(Math.random() * noun.length)] + domain[Math.floor(Math.random() * domain.length)];
+    var con = pronoun[Math.floor(Math.random() * pronoun.length)] + adj[Math.floor(Math.random() * adj.length)] + noun[Math.floor(Math.random() * noun.length)] + domain[Math.floor(Math.random() * domain.length)];
     
-    return container;
+    return con;
 }
 
 console.log(ranDom());
